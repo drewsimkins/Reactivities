c7a74d9a0411bdf19e83ef1ee7d0cd7e25806e7a
Drew Simkins <andrewsimk@gmail.com>
2021-05-18 15:47:31 +0100

    Parway through section 5

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Grid, GridColumn } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+import ActivityDetails from "../details/ActivityDetails";
+import ActivityList from "./ActivityList";
+import ActivityForm from "../form/ActivityForm";
+
+interface Props {
+  activities: Activity[];
+  selectedActivity: Activity | undefined;
+  selectActivity: (id: string) => void;
+  cancelSelectActivity: () => void;
+}
+
+export default function ActivityDashoard({
+  activities,
+  selectActivity,
+  selectedActivity,
+  cancelSelectActivity,
+}: Props) {
+  return (
+    <Grid>
+      <Grid.Column width="10">
+        <ActivityList activities={activities} selectActivity={selectActivity} />
+      </Grid.Column>
+      <GridColumn width="6">
+        {selectedActivity && (
+          <ActivityDetails
+            activity={selectedActivity}
+            cancelSelectActivity={cancelSelectActivity}
+          />
+        )}
+        <ActivityForm />
+      </GridColumn>
+    </Grid>
+  );
+}
