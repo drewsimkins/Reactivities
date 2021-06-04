5758959ee18de810bd427dd0ef188c8c54a3fcff
Drew Simkins <andrewsimk@gmail.com>
2021-06-04 14:35:21 +0100

    Added Styling to the project

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -4,6 +4,7 @@ import ActivityList from "./ActivityList";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
+import ActivityFilters from "./ActivityFilters";
 
 export default observer(function ActivityDashoard() {
   const { activityStore } = useStore();
@@ -22,7 +23,7 @@ export default observer(function ActivityDashoard() {
         <ActivityList />
       </Grid.Column>
       <GridColumn width="6">
-        <h2>Activity filters</h2>
+        <ActivityFilters />
       </GridColumn>
     </Grid>
   );
