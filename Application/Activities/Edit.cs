71b5252869baa9da5d439dc3d409f95c6c61b20e
Drew Simkins <andrewsimk@gmail.com>
2021-06-04 15:04:33 +0100

    Implemented FluidValidation for Creation & Editing of Activities in the API

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using Domain;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
+using FluentValidation;
 using Persistence;
 
 namespace Application.Activities
@@ -13,6 +13,14 @@ namespace Application.Activities
         public class Command : IRequest
         {
             public Activity Activity { get; set; }
+        }
+
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.Activity).SetValidator(new ActivityValidator());
+            }
         }
 
         public class Handler : IRequestHandler<Command>
